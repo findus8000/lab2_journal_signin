5b09ae21a6860c7e617627a6262b507f7fec8a9b
findus8000 <joelpriore@gmail.com>
2024-11-29 16:45:40 +0100

    kuberneetes adjustments

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM eclipse-temurin:23-jdk-alpine
 WORKDIR /app
 EXPOSE 8080
-COPY target/messages-0.0.1-SNAPSHOT.jar /app.jar
+COPY target/lab2_journal_signin-0.0.1-SNAPSHOT.jar /app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/kth/lab2_journal_signin/CorsConfig.java
+++ src/main/java/kth/lab2_journal_signin/CorsConfig.java
@@ -9,10 +9,14 @@ public class CorsConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                        "http://react-frontend:3000",  // Frontend service in Kubernetes
+                        "http://localhost:3000"         // For local testing
+                )
                 .allowedMethods("GET", "POST")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
 }
+
